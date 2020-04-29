b575b7877fd71e5145c30c4917e366a8620ea4bc
tunapanini <haneuneee@gmail.com>
2020-04-29 14:59:49 +0900

    1 file modified

--- index.tsx
+++ index.tsx
@@ -3,29 +3,17 @@ import { render } from 'react-dom';
 import Hello from './Hello';
 import './style.css';
 
-interface AppProps { }
-interface AppState {
-  name: string;
-}
+function App() {
+  const name = 'React';
 
-class App extends Component<AppProps, AppState> {
-  constructor(props) {
-    super(props);
-    this.state = {
-      name: 'React'
-    };
-  }
-
-  render() {
-    return (
-      <div>
-        <Hello name={this.state.name} />
-        <p>
-          Start editing to see some magic happen :)
-        </p>
-      </div>
-    );
-  }
+  return (
+    <div>
+      <Hello name={name} />
+      <p>
+        Start editing to see some magic happen :)
+      </p>
+    </div>
+  );
 }
 
 render(<App />, document.getElementById('root'));
